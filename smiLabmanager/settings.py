69350b2541268dc02286415b5db816b2daa7c49b
Mitch Alexander <mitchella099@gmail.com>
2023-07-18 11:24:54 +1000

    security key

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u46n(!5fnq*m-8nx+z2fm+=!w%#)6if63p)j(+s21vf(m$@^9#'
+#SECRET_KEY = 'django-insecure-u46n(!5fnq*m-8nx+z2fm+=!w%#)6if63p)j(+s21vf(m$@^9#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
