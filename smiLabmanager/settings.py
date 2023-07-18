cebd510e017770ebcbe8914019d0eefbcdb1e495
Mitch Alexander <mitchella099@gmail.com>
2023-07-18 13:26:38 +1000

    testing

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
-
+CSRF_TRUSTED_ORIGINS = ["<railway URL you will get later on deployment>"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -82,22 +82,17 @@ WSGI_APPLICATION = 'smiLabmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#         'ENGINE': 'django.db.backends.postgressql',
-#        'NAME': 'railway',
-#        'USER': 'postgres',
-#       'PASSWORD': 'WYmzMwj86TpCU4fGcGmK',
-#       'HOST': 'containers-us-west-173.railway.app',
-#       'PORT': '5566',
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("PGDATABASE"),
+        'USER': os.environ.get("PGUSER"),
+        'PASSWORD': os.environ.get("PGPASSWORD"),
+        'HOST': os.environ.get("PGHOST"),
+        'PORT': os.environ.get("PGPORT"),
     }
 }
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
