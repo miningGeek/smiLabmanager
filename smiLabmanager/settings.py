d8ae06025423ac0591649011117a8449c7f0376a
Mitch Alexander <mitchella099@gmail.com>
2023-07-18 12:28:51 +1000

    psycopg2

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -82,17 +82,17 @@ WSGI_APPLICATION = 'smiLabmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-import dj_database_url
-DATABASE_URL = 'postgresql://postgres:CABcdnHQ58Sa4oIDrnO8@containers-us-west-64.railway.app:5787/railway'
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+  'default': {
+        'ENGINE': 'django.db.backends.postgressql',
+       'NAME': '<PGDATABASE>',
+       'USER': '<PGUSER>',
+      'PASSWORD': '<PGPASSWORD>',
+      'HOST': '<PGUSER>',
+      'PORT': '<PGPORT>',
+    }
 }
+
 
 
 # Password validation
