b095b89229979f689cf9467a0b962ea9f0439798
Mitch Alexander <mitchella099@gmail.com>
2023-07-18 12:55:28 +1000

    update

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -82,17 +82,25 @@ WSGI_APPLICATION = 'smiLabmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#         'ENGINE': 'django.db.backends.postgressql',
+#        'NAME': 'railway',
+#        'USER': 'postgres',
+#       'PASSWORD': 'WYmzMwj86TpCU4fGcGmK',
+#       'HOST': 'containers-us-west-173.railway.app',
+#       'PORT': '5566',
+#     }
+# }
 DATABASES = {
-  'default': {
-        'ENGINE': 'django.db.backends.postgressql',
-       'NAME': 'railway',
-       'USER': 'postgres',
-      'PASSWORD': 'WYmzMwj86TpCU4fGcGmK',
-      'HOST': 'containers-us-west-173.railway.app',
-      'PORT': '5566',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
