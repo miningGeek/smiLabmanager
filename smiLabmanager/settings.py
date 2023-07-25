bc4a73b1079ad598b7115f8ca75c595d632cfaa0
miningGeek <115459317+miningGeek@users.noreply.github.com>
2023-07-25 18:33:28 +1000

    Update settings.py

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -156,5 +156,5 @@ STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-AWS_S3_FILE_OVERWITE = False
+AWS_S3_FILE_OVERWRITE = False
 
