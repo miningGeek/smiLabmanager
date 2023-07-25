7ed30a3d9c9096891b9bd840f58fd368cbe5569f
miningGeek <115459317+miningGeek@users.noreply.github.com>
2023-07-25 20:15:59 +1000

    Update settings.py

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['smilabmanager-production-f50c.up.railway.app']
 
-#CSRF_TRUSTED_ORIGINS = [https://smilabmanager-production-f50c.up.railway.app]
+CSRF_TRUSTED_ORIGINS = ['https://smilabmanager-production-f50c.up.railway.app']
 
 # Application definition
 
