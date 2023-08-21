8f55aa7f9718f1f55e6891aba3fcfe472b3da52c
Mitch Alexander <mitchella099@gmail.com>
2023-08-21 20:20:35 +1000

    fix security

--- smiLabmanager/settings.py
+++ smiLabmanager/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
